e6f29a2cfba2a0574a7e00565525dfe9d520d9ae
이동준 <dongjun970603@gmail.com>
2023-12-10 02:07:57 +0900

    update useNavigate and Link tag

--- naras/src/App.jsx
+++ naras/src/App.jsx
@@ -1,18 +1,32 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Country from './pages/Country';
 import NotFound from './pages/NotFound';
 
 function App() {
+  const nav = useNavigate();
+
+  const onClick = () => {
+    nav('/search');
+  };
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/search' element={<Search />}></Route>
-      <Route path='/country' element={<Country />}></Route>
-      <Route path='*' element={<NotFound />}></Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+        <Route path='/country' element={<Country />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/search'}>Search</Link>
+        <Link to={'/country'}>Country</Link>
+
+        <button onClick={onClick}>서치 페이지로 이동</button>
+      </div>
+    </>
   );
 }
 
