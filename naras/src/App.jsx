0a49ab029c838ea8144625c1e1fda661c25e7d66
이동준 <dongjun970603@gmail.com>
2023-12-10 02:03:34 +0900

    Route setting

--- naras/src/App.jsx
+++ naras/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Country from './pages/Country';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/search' element={<Search />}></Route>
+      <Route path='/country' element={<Country />}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
