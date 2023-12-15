435a2143b7a8dccc8ec3401bfaa22135d0c0ffef
이동준 <dongjun970603@gmail.com>
2023-12-15 20:16:52 +0900

    useSSR

--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -1,36 +1,11 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-
-export default function Home() {
-  const code = 'KOR';
-
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push({
-      pathname: '/country/[code]',
-      query: { code },
-    });
-  };
-  return (
-    <div>
-      Home Page
-      <div>
-        <button onClick={onClickButton}>Search</button>
-      </div>
-      <div>
-        <Link href={'/search'}>Search</Link>
-      </div>
-      <div>
-        <Link
-          href={{
-            pathname: '/country/[code]',
-            query: { code: code },
-          }}
-        >
-          {code}
-        </Link>
-      </div>
-    </div>
-  );
+export default function Home({ name }) {
+  return <div>{name}</div>;
 }
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      name: 'KOREA',
+    },
+  };
+};
