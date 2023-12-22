3a957da38aedd13441ea4ece8bcea060c808e847
이동준 <dongjun970603@gmail.com>
2023-12-22 17:40:13 +0900

    fallback option setting / blocking and true

--- first-next-app/pages/country/[code].js
+++ first-next-app/pages/country/[code].js
@@ -6,6 +6,14 @@ export default function Country({ country }) {
   const router = useRouter();
   const { code } = router.query;
 
+  if (router.isFallback) {
+    return <div>Loading . . .</div>;
+  }
+
+  if (!country) {
+    return <div>존재하지 않는 국가입니다</div>;
+  }
+
   return (
     <div>
       {country.commonName} {country.officialName}
@@ -18,7 +26,7 @@ Country.Layout = SubLayout;
 export const getStaticPaths = async () => {
   return {
     paths: [{ params: { code: 'ABW' } }, { params: { code: 'KOR' } }],
-    fallback: false,
+    fallback: true, // data가 비어있는 fallback 상태의 html을 먼저 받은 뒤, props를 전달 받음
   };
 };
 
